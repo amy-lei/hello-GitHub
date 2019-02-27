2030870b70ad81597da3217abd9e53e12013a265
Amy Lei <amy.lei2001@yahoo.com>
2019-02-26 21:15:50 -0500

    commit message

---
+++ Greeter.java
@@ -0,0 +1,18 @@
+// Greets a person without repetition
+
+public class Greeter {
+
+    
+    public static void main(String[] person) {                        
+        greet( "Professor Kernighan");
+        greet( "Admiral Hopper");
+        greet( "Good-time Johnny");
+    }
+    /*
+    One cannot define greet in the function body of another method. However, it is legal to define greet both before the main method or after the main method. 
+    */
+    public static void greet( String person) {
+            System.out.println( "hello, " + person + "! How you doon?");
+        }
+
+}
